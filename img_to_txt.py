178e9a95227b8fe98d21cbab522fb44c67454f8a
Vardhan-R <82092439+Vardhan-R@users.noreply.github.com>
2023-02-22 20:05:11 +0530

    Update img_to_txt.py

--- img_to_txt.py
+++ img_to_txt.py
@@ -1,15 +1,16 @@
 from PIL import Image
 
-for i in range(20):
-    im = Image.open(f"ai_dataset/test_images_formatted/test_img_{i}_category_{int(4 < i < 15)}.png")
+for i in range(1):
+    im = Image.open(f"C:/Users/vrdhn/Desktop/CS/ai_dataset_2/category_0/category_0_img_2.jpg")
     pixels_lst = list(im.getdata())
     img_size = im.size # width (x-axis), height (y-axis)
 
-    txt_file = open(f"ai_dataset/test_images_formatted/all_test_images_formatted.txt", 'a')
+    txt_file = open(f"C:/Users/vrdhn/Desktop/CS/test_file_1.txt", 'w')
 
     txt_file.write(f"{img_size[0]} {img_size[1]}\n")
 
     for j in range(img_size[0] * img_size[1]):
-        txt_file.write(" ".join([str(x) for x in pixels_lst[j]]) + '\n')
+        print(j, img_size[0] * img_size[1])
+        txt_file.write(";".join([str(x) for x in pixels_lst[j]]) + 'm\n')
 
     txt_file.close()
