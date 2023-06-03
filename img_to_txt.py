77fc291af05debea971dbc74ea47dbbadeabd22e
Vardhan-R <82092439+Vardhan-R@users.noreply.github.com>
2023-06-03 21:17:23 +0530

    Update img_to_txt.py

--- img_to_txt.py
+++ img_to_txt.py
@@ -1,16 +1,22 @@
 from PIL import Image
 
+rating_int = 0
+
 for i in range(1):
-    im = Image.open(f"C:/Users/vrdhn/Desktop/CS/ai_dataset_2/category_0/category_0_img_2.jpg")
+    im = Image.open(f"tst_img_1.png")
     pixels_lst = list(im.getdata())
+    im.close()
     img_size = im.size # width (x-axis), height (y-axis)
 
-    txt_file = open(f"C:/Users/vrdhn/Desktop/CS/test_file_1.txt", 'w')
+    pixel_cnt = img_size[0] * img_size[1]
 
-    txt_file.write(f"{img_size[0]} {img_size[1]}\n")
+    str_to_write = f"{img_size[0]} {img_size[1]}\n"
 
-    for j in range(img_size[0] * img_size[1]):
-        print(j, img_size[0] * img_size[1])
-        txt_file.write(";".join([str(x) for x in pixels_lst[j]]) + 'm\n')
+    for j in range(pixel_cnt):
+        # txt_file.write(";".join([str(x) for x in pixels_lst[j]]) + 'm\n')
 
+        str_to_write += " ".join([str(x) for x in pixels_lst[j][:3]]) + "\n"
+        print(f"{j + 1} / {pixel_cnt}")
+    txt_file = open(f"test_file_1.txt", 'w')
+    txt_file.write(str_to_write)
     txt_file.close()
